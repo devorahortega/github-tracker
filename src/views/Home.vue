38a1b0633c1c3b365e15f403a56fd658d8df7adc
Devorah Ortega <devorahb.ortega@gmail.com>
2022-01-13 21:52:23 -0500

    Adds axios and vue2 filters as well as the method to draw in the info from the 5 repos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>THIS IS THE GITHUB TRACKER</h1>
+    <div v-for="repo in repos" v-bind:key="repo.id">
+      <p>{{ repo.name }}</p>
+      <p></p>
+      <p></p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      repos: [],
+    };
+  },
+  methods: {
+    repoIndex: function () {
+      axios.get("https://github.com/repos/vuejs/vue").then((response) => {
+        this.repos.push(response.data);
+      });
+      axios.get("https://github.com/repos/angular/angular.js").then((response) => {
+        this.repos.push(response.data);
+      });
+      axios.get("https://github.com/repos/emberjs/ember.js").then((response) => {
+        this.repos.push(response.data);
+      });
+      axios.get("https://github.com/repos/sveltejs/svelte").then((response) => {
+        this.repos.push(response.data);
+      });
+      axios.get("https://github.com/repos/facebook/react").then((response) => {
+        this.repos.push(response.data);
+      });
+    },
   },
 };
 </script>
